c95693d24ef6c1cbd3f1ceba7ddfde330cf439f8
Dios <lockpickman@outlook.com>
2019-09-01 04:06:37 -0500

    Druken master

---
+++ New Unity Project/Assets/MainChar.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainChar : MonoBehaviour
+{
+    public float velocidad;
+    public Rigidbody2D rb;
+    //public Animator anim;
+    Vector2 mvmt;
+    private Vector2 lookAt;
+    public float dashVel;
+    private float dashTime;
+    public float dashDur;
+    public float coolDown;
+    private float vel;
+    private float cdTime;
+    //public float OffsetColider=2.0f;
+    //public float Reach = 2.1f;
+    void Start()
+    {
+        //anim=this.GetComponent<Animator>();
+        rb=this.GetComponent<Rigidbody2D>();
+        rb.constraints=RigidbodyConstraints2D.FreezeRotation;
+    }
+    void Update()
+    {
+        /*animator
+        anim.SetFloat("Horizontal",mvmt.x);
+        anim.SetFloat("Vertical",mvmt.y);
+        */
+        mvmt.Set(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        
+    }
+    void FixedUpdate() {
+        //RaycastCheckUpdate();
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time>cdTime){
+            if(dashTime<=0){
+                dashTime=dashDur;
+                vel=velocidad;
+            }else{
+                dashTime-=Time.deltaTime;
+                vel=dashVel;
+                cdTime=coolDown+Time.time;
+            }
+        }else{
+            vel=velocidad;
+        }
+        rb.MovePosition(rb.position + mvmt.normalized * vel * Time.fixedDeltaTime);
+    }
+    /* ray-cast para saber si esta volteando si sirve pero falta el animator bien echo -Dios
+    
+        public RaycastHit2D CheckRaycast(Vector2 dir)
+    {
+        float dirOffset = OffsetColider * (dir.x > 0 ? 1 : -1);
+
+        Vector2 org = new Vector2(transform.position.x + dirOffset, transform.position.y);
+
+        return Physics2D.Raycast(org, dir, Reach);
+    }
+    private bool RaycastCheckUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector2 lookAt = new Vector2(anim.GetFloat("Horizontal"), anim.GetFloat("Vertical"));
+            RaycastHit2D hit = CheckRaycast(lookAt);
+            
+            if (hit.collider)
+            {
+                Debug.Log("Hit-" + hit.collider.name);
+
+                Debug.DrawRay(transform.position, hit.point, Color.red, 3f);
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }*/
+}
+
---
+++ New Unity Project/Assets/MusicBox.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBox : MonoBehaviour
+{
+    private bool pickUpAllowed;
+    private bool grabbed;
+    public float distance=2f;
+    public Collider2D grab;
+    public Collider2D hitbox;
+    public Collider2D AoE;
+    public float throwforce;
+    void Start(){
+        grabbed=false;
+    }
+
+    void Update () {
+
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E)&&(grabbed==false)){
+            PickUp();
+        }
+        if(grabbed){
+            this.transform.position=GameObject.Find("Lucy").transform.position;
+        }
+        if (grabbed && Input.GetKeyDown(KeyCode.E)){
+            Throw();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.name.Equals("Lucy")){
+            pickUpAllowed = true;
+        }        
+    }
+    private void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.name.Equals("Lucy")){
+            pickUpAllowed = false;
+        }
+    }
+    private void PickUp(){
+        grabbed=true;
+        hitbox.enabled=false;
+        Debug.Log("grabbed");
+    }
+    private void Throw(){
+        grabbed=false;
+        this.transform.position=GameObject.Find("Lucy").transform.position*throwforce;
+    }
+}

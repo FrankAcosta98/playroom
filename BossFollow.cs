1c4827deb4c7b025cbcbb3b006dd904f52f09e6f
MikeGPQ <54824814+MikeGPQ@users.noreply.github.com>
2019-09-11 15:07:48 -0500

    Add files via upload

---
+++ BossFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFollow : MonoBehaviour {
+
+    public float velocidad;
+
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, velocidad * Time.deltaTime);
+		
+	}
+}

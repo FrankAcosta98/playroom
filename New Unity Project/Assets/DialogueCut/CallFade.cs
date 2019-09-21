4057cbf0f51a5fe9b339fdd1a60bc9c60e668fd7
RoliCorrea <54562083+RoliCorrea@users.noreply.github.com>
2019-09-20 20:32:11 -0500

    Add files via upload

---
+++ New Unity Project/Assets/DialogueCut/CallFade.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CallFade : MonoBehaviour
+{
+
+    // the image you want to fade, assign in inspector
+    public Image img;
+
+    public void Start()
+    {
+        // fades the image out when you click
+        StartCoroutine(FadeImage(true));
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+    }
+}

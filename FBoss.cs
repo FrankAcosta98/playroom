a7b99f30cede57e9a921e06bc85d43fedc653150
Dios <lockpickman@outlook.com>
2019-10-13 21:47:54 -0500

    --- Auto-Git Commit ---

--- FBoss.cs
+++ FBoss.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +8,6 @@ public class FBoss : MonoBehaviour
 {
     [SerializeField]
     private Transform[] points;
-    private GameObject[] boxes; 
-    [SerializeField]
     public float speed;
     public float cooldown;
     private int Cpoint = 0;
@@ -16,93 +16,79 @@ public class FBoss : MonoBehaviour
     private bool chilling = false;
     public float chill;
     private float chillLevel;
-    public float AngerLvl;
-    private float anger;
-    private int fase;
-    public Collider2D CdV;
+    public CircleCollider2D detect;
     public Rigidbody2D rb;
-    
-    void Start(){
+
+    void Start()
+    {
         wait = cooldown;
         transform.position = points[Cpoint].transform.position;
         rb = this.GetComponent<Rigidbody2D>();
-        chillLevel = chill;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        chillLevel = chill;
     }
-    // Update is called once per frame
-void Update()
+    void Update()
     {
-        if (anger>=AngerLvl)
-            Puch();
-        else if (hmm)
+        if (hmm) {
             Hunt();
+        }
         else
             Move();
-        if (chillLevel <= 0){
+
+        if (chillLevel <= 0) { 
             hmm = false;
-            GameObject.FindGameObjectWithTag("Focus").tag = "detectable";
-            }
-        if (chilling)
-            chillLevel -= Time.deltaTime;
-        anger+=Time.deltaTime;
-    }
-    private void Puch(){
-        //lights out
-        //https://www.youtube.com/watch?v=kTvBRkPTvRY
-        //https://www.youtube.com/watch?v=tdSmKaJvCoA
-        transform.position = points[Cpoint].transform.position;
-        switch (fase) 
-        {
-            case 1:{// 4 cajas random la primera caja es usable
-                GameObject[] boxes = new GameObject[4];
-            }break;
-            case 2:{// 5 cajas radom la primera caja es usable
-                GameObject[] boxes = new GameObject[5];
-            }break;
-            case 3:{// 6 cajas random la primeera es usable y ultima llave
-                GameObject[] boxes = new GameObject[6];
-            fase=1; //para que de 2 osea la ultima fase se repite
-            }break;
+            chilling = false;
+            if(MainChar.instace.GetComponent<MainChar>().tag=="Focus")
+                MainChar.instace.GetComponent<MainChar>().tag="detectable";
         }
-        //spawnear cosas random en el Vect2 area
-        fase++;
-        anger=0.0f;
+        if (chilling)
+        {
+            chillLevel -= Time.deltaTime;
+        }
     }
-    private void Move(){
+    private void Move()
+    {
         transform.position = Vector2.MoveTowards(transform.position, points[Cpoint].position, speed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position, points[Cpoint].position) < 0.2f){
-            if (wait <= 0){
+        if (Vector2.Distance(transform.position, points[Cpoint].position) < 0.2f)
+        {
+            if (wait <= 0)
+            {
                 if (Cpoint + 1 == points.Length)
                 {
                     Cpoint = 0;
                 }
-                else{
+                else
+                {
                     Cpoint += 1;
                 }
                 wait = cooldown;
             }
-            else{
+            else
+            {
                 wait -= Time.deltaTime;
             }
         }
     }
-    private void OnTriggerEnter2D(Collider2D prey){
-        if (prey.gameObject.transform.tag == "detectable" && prey.GetType() == typeof(CircleCollider2D)){
+    private void OnTriggerEnter2D(Collider2D prey)
+    {
+        if (prey.gameObject.transform.tag == "detectable")
+        {
             hmm = true;
             prey.gameObject.tag = "Focus";
             chillLevel = chill;
-        }
-
-        if (prey.gameObject.transform.tag == "Focus" && prey.GetType() == typeof(BoxCollider2D)){
-            Debug.Log("Ya te cargo el payaso");
-            Destroy(prey.gameObject);
+            //
+            Debug.Log(prey.gameObject.transform.tag);
         }
     }
-    private void OnTriggerExit2D(Collider2D prey){
+    private void OnTriggerExit2D(Collider2D prey)
+    {
         chilling = true;
+        
     }
-    private void Hunt(){
-        transform.position = Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Focus").transform.position, speed * Time.deltaTime);
+    private void Hunt()
+    {
+        if(GameObject.FindGameObjectWithTag("Focus")!=null)
+            transform.position = Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Focus").transform.position, speed * Time.deltaTime);
     }
 }

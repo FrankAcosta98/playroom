edf6cda87a2accbf929d307772269609e3d0e97b
MikeGPQ <54824814+MikeGPQ@users.noreply.github.com>
2019-10-15 11:52:20 -0500

    Add files via upload

--- Llave.cs
+++ Llave.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class Llave : MonoBehaviour
 {
     private bool usable = false;
+
+    void Start()
+    {
+
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && usable == true)
         {
-            MainChar.instace.GetComponent<MainChar>().hasKey = true;
+            MainChar.instace.gameObject.GetComponent<MainChar>().hasKey = true;
             Destroy(gameObject);
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name.Equals("Lucy")&&other.gameObject.GetComponent<MainChar>().RaycastCheckUpdate())
+        if (other.gameObject.name.Equals("Lucy")  /* Agregar Raycast*/)
         {
             usable = true;
+           
         }
     }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("Lucy")  /* Agregar Raycast*/)
+        {
+            usable = false;
+
+        }
+    }
 }

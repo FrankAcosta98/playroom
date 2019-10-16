1ffd71cc8a6c89ee8de74918d462a9ce57ce51ad
RoliCorrea <54562083+RoliCorrea@users.noreply.github.com>
2019-10-16 18:20:59 -0500

    Add files via upload

--- EnemyStates.cs
+++ EnemyStates.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EnemyStates : MonoBehaviour
 {
     CircleCollider2D coli;
-    public float rad;
+    public float rad; //Valor del radio de detección
     public GameObject target;
-    public float moveSpeed = 15;
-    public float rotationSpeed = 5;
+    public float moveSpeed = 15; //Velocidad a la que se mueve
+    public float rotationSpeed = 5; //Velocidad a la que rota
     private Rigidbody2D rb;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,34 +23,36 @@ public class EnemyStates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (target != null) //Si target existe...
         {
+            //El monstruo rotara hacia este y se moverá a su posición
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), rotationSpeed * Time.deltaTime);
-            
+
             transform.position += transform.forward * Time.deltaTime * moveSpeed;
 
-            
+
 
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //El metodo sirve para determinar si es Lucy o una caja
     {
-        if (collision.gameObject.tag.Equals("Lucy"))
+        if (collision.gameObject.name.Equals("Lucy"))
         {
             target = collision.gameObject;
             Debug.Log("Persiguiendo algo");
 
-        } else if (collision.gameObject.tag.Equals("MusicBox"))
+        }
+        else if (collision.gameObject.name.Equals("MusicBox"))
         {
             target = collision.gameObject;
             Debug.Log("Persiguiendo algo");
         }
     }
-    
-    
 
-    private void OnTriggerExit2D(Collider2D collision)
+
+
+    private void OnTriggerExit2D(Collider2D collision) //Si te alejas ya no hay target
     {
         target = null;
         Debug.Log("Ya no persigo a nadie");

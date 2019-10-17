e4d2b2133c60b0f7a7aadb1964b55c93a83e0832
RoliCorrea <54562083+RoliCorrea@users.noreply.github.com>
2019-10-16 19:00:09 -0500

    Add files via upload

--- BossScout.cs
+++ BossScout.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossScout : MonoBehaviour {
+public class BossScout : MonoBehaviour
+{
 
     [Header("Variables")]
-    public float cooldown;
-    public float velocidad;
+    public float cooldown; //Tiempo para regresar a cooldown
+    public float velocidad; //Velocidad de movimiento
 
     private float Aceleracion;
     private Vector2 movementDirection;
     private Vector2 movementPerSecond;
-    
+
 
     void Start()
     {
@@ -21,19 +22,19 @@ public class BossScout : MonoBehaviour {
 
     void CalcuateNewMovementVector()
     {
-        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
-        movementPerSecond = movementDirection * velocidad;
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized; //Se elige un punto aleatorio dentro del rango
+        movementPerSecond = movementDirection * velocidad; //Movimiento
     }
 
 
     void Update()
     {
-        if (Time.time - Aceleracion > cooldown)
+        if (Time.time - Aceleracion > cooldown) //Se calcula si el tiempo es mayor al cooldown para moverse
         {
             Aceleracion = Time.time;
             CalcuateNewMovementVector();
         }
         transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y + (movementPerSecond.y * Time.deltaTime));
-        
+
     }
 }

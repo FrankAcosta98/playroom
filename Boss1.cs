aa03cd4ddbfd18c2c8f522fe37673739e0d3f146
RoliCorrea <54562083+RoliCorrea@users.noreply.github.com>
2019-10-16 21:30:51 -0500

    Add files via upload

--- Boss1.cs
+++ Boss1.cs
@@ -7,16 +7,16 @@ using UnityEngine;
 public class Boss1 : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] points;
-    public float speed;
-    public float cooldown;
-    private int Cpoint = 0;
+    private Transform[] points; //Arreglo de puntos a los que se moverá
+    public float speed; 
+    public float cooldown; //Tiempo para cambiar
+    private int Cpoint = 0; //Indice de puntos
     private float wait;
     private bool hmm = false;
     private bool chilling = false;
-    public float chill;
+    public float chill; //Tiempo para regresar a chilling
     private float chillLevel;
-    public CircleCollider2D detect;
+    public CircleCollider2D detect; //Detección de Lucy
     public Rigidbody2D rb;
 
     void Start()
@@ -29,66 +29,68 @@ public class Boss1 : MonoBehaviour
     }
     void Update()
     {
-        if (hmm) {
-            Hunt();
+        if (hmm)
+        {
+            Hunt(); //Se llama el método Hunt cuando se entra en estado de hmm
         }
-        else
+        else //Si no se está en hmm se sigue moviendo
             Move();
 
-        if (chillLevel <= 0) { 
-            hmm = false;
-            chilling = false;
-            if(MainChar.instace.GetComponent<MainChar>().tag=="Focus")
-                MainChar.instace.GetComponent<MainChar>().tag="detectable";
-        }
-        if (chilling)
+        if (chillLevel <= 0) //Cuando chillLevel se encuentra bajo 0...
         {
-            chillLevel -= Time.deltaTime;
+            hmm = false; //Se resetea los booleanos
+            chilling = false;
+            if (MainChar.instace.GetComponent<MainChar>().tag == "Focus")
+                MainChar.instace.GetComponent<MainChar>().tag = "detectable"; //Y Lucy vuelve a ser detectable
+        }
+        if (chilling) //Si estás en chilling
+        {
+            chillLevel -= Time.deltaTime; //Se reduce
         }
     }
-    private void Move()
+    private void Move() //Método que sirve para patrullar
     {
-        transform.position = Vector2.MoveTowards(transform.position, points[Cpoint].position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, points[Cpoint].position, speed * Time.deltaTime); //Aquí se indica que punto del arreglo se moverá
 
-        if (Vector2.Distance(transform.position, points[Cpoint].position) < 0.2f)
+        if (Vector2.Distance(transform.position, points[Cpoint].position) < 0.2f) //Cuando te acercas
         {
-            if (wait <= 0)
+            if (wait <= 0) //Cuando llegas a 0..
             {
-                if (Cpoint + 1 == points.Length)
+                if (Cpoint + 1 == points.Length)//Si llegas al limite se resetea a 0
                 {
                     Cpoint = 0;
                 }
-                else
+                else //Si no se suma 1 para pasar al siguiente
                 {
                     Cpoint += 1;
                 }
                 wait = cooldown;
             }
-            else
+            else //Si no has llegado a 0
             {
-                wait -= Time.deltaTime;
+                wait -= Time.deltaTime; //Reduces el tiempo
             }
         }
     }
-    private void OnTriggerEnter2D(Collider2D prey)
+    private void OnTriggerEnter2D(Collider2D prey) //Cuando te detectan
     {
-        if (prey.gameObject.transform.tag == "detectable")
+        if (prey.gameObject.transform.tag == "detectable") 
         {
-            hmm = true;
-            prey.gameObject.tag = "Focus";
-            chillLevel = chill;
+            hmm = true; //El boss entra en hmm
+            prey.gameObject.tag = "Focus"; //Te vuelves un focus
+            chillLevel = chill; //Se empieza el chill
             //
             Debug.Log(prey.gameObject.transform.tag);
         }
     }
-    private void OnTriggerExit2D(Collider2D prey)
+    private void OnTriggerExit2D(Collider2D prey) //Cuando te alejas
     {
-        chilling = true;
-        
+        chilling = true; //Te calmas
+
     }
-    private void Hunt()
+    private void Hunt() //Método para cazar
     {
-        if(GameObject.FindGameObjectWithTag("Focus")!=null)
-            transform.position = Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Focus").transform.position, speed * Time.deltaTime);
+        if (GameObject.FindGameObjectWithTag("Focus") != null) //Cuando existe prey
+            transform.position = Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Focus").transform.position, speed * Time.deltaTime);//Te diriges al objeto de focus
     }
 }

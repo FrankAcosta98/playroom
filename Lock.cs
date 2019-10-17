5301c7d23f10ac4e82d2d870875bfa9d34de593c
MikeGPQ <54824814+MikeGPQ@users.noreply.github.com>
2019-10-17 13:59:36 -0500

    Add files via upload

--- Lock.cs
+++ Lock.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Lock : MonoBehaviour
 {
     //El código sirve para verificar si Lucy tiene la llave y está en rango de poder interactuar con la caja de fusibles
+
+    [Header("Components")]
+    public Sprite seeing;
+    public Sprite notSeeing;
+
     private bool block = true;
 
     void Start()
@@ -22,16 +27,22 @@ public class Lock : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name.Equals("Lucy") && MainChar.instace.gameObject.GetComponent<MainChar>().hasKey == true) //Si se tiene la llave se desbloquea y es posible usarlo
+        if (other.gameObject.name.Equals("Lucy") && MainChar.instace.RaycastCheckUpdate())
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = seeing;
+        }
+
+        if (other.gameObject.name.Equals("Lucy") && MainChar.instace.gameObject.GetComponent<MainChar>().hasKey == true)  //Si se tiene la llave se desbloquea y es posible usarlo
         {
             block = false;
         }
     }
 
-    void OnTriggerExit2D(Collider2D other) //Si Lucy se aleja no se puede usar
+    void OnTriggerExit2D(Collider2D other) //Si Lucy se aleja no se puede usa
     {
-        if (other.gameObject.name.Equals("Lucy")  /* Agregar Raycast*/)
+        if (other.gameObject.name.Equals("Lucy") || !MainChar.instace.RaycastCheckUpdate())
         {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = notSeeing;
             block = true;
         }
     }

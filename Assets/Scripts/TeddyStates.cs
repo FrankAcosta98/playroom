c7659c67359197635756016978b6e3971b07521b
FrankAcosta <frank-nova98@live.com>
2019-11-25 01:17:14 -0600

    --- Auto-Git Commit ---

--- Assets/Scripts/TeddyStates.cs
+++ Assets/Scripts/TeddyStates.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿
+using System.Linq.Expressions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,36 +21,36 @@ public class TeddyStates : MonoBehaviour
     {
         //Se inician los componentes necesarios
         col = GetComponent<CircleCollider2D>();
-        lt=GetComponent<Light2D>();
+        lt = GetComponent<Light2D>();
         col.radius = sizeRad;
-        lt.pointLightOuterRadius=0.68f;
+        lt.pointLightOuterRadius = 0.68f;
         tmpv = this.GetComponentInParent<MainChar>().velocidad;
         transform.gameObject.tag = "detectable"; //Lucy se establece como detectable
     }
 
     void Update()
     {
-        
+
         if (this.GetComponentInParent<MainChar>().hasBox == false) //Si Lucy no tiene una caja...
         {
-            
+
             if (Input.GetKeyDown(KeyCode.F) && (IsLightOn == false)) //Si se apreta "F" y la luz está apagada...
             {
                 col.radius = RadMultWithLight; //Se aumenta el radio al número definido
-                lt.pointLightOuterRadius=1.36f;
+                lt.pointLightOuterRadius = 1.36f;
                 IsLightOn = true; //Se establece que la luz está prendida
             }
-            else if (Input.GetKeyDown(KeyCode.F) && (IsLightOn == true))Si se apreta "F" y la luz está prendida...
+            else if (Input.GetKeyDown(KeyCode.F) && (IsLightOn == true)) //Si se apreta "F" y la luz está prendida...
             {
                 col.radius = sizeRad; //El radio vuelve al tamaño original
-            lt.pointLightOuterRadius=0.68f;
+                lt.pointLightOuterRadius = 0.68f;
                 IsLightOn = false; //Se establece que la luz está apagada
             }
 
             if (Input.GetKey(KeyCode.LeftShift) && IsLightOn == true && Holded == false) //Si se mantiene apretado LeftShift, la luz está prendida y no se está levantando el Teddy
             {
                 col.radius = RadMultWithHold; //El radio del collider crece al número definido
-                lt.pointLightOuterRadius=2.72f;
+                lt.pointLightOuterRadius = 2.72f;
                 Holded = true; //Se establece que Teddy está levantado
                 this.GetComponentInParent<MainChar>().velocidad = VelocidadLenta; //La velocidad de Lucy se reduce a lo establecido
                 this.GetComponentInParent<Animator>().SetBool("High", true); //El Animator se activa para la animación de levantar a Teddy
@@ -57,17 +58,17 @@ public class TeddyStates : MonoBehaviour
             else if (Input.GetKeyUp(KeyCode.LeftShift) && Holded == true) //Si se deja de apretar LeftShift y se está levantando a Teddy...
             {
                 col.radius = RadMultWithLight; //El radio regresa al número con la luz prendida
-                lt.pointLightOuterRadius=1.36f;
+                lt.pointLightOuterRadius = 1.36f;
                 Holded = false; //Se establece que no se está levantando a Teddy
                 this.GetComponentInParent<Animator>().SetBool("High", false); //El Animator ya no realiza la animación de Teddy levantado
                 this.GetComponentInParent<MainChar>().velocidad = tmpv; //La velocidad de Lucy regresa a la normal
             }
         }
-        else// En otro caso...
+        else //En otro caso...
         {
-            col.radius = sizeRad;//El radio se mantiene al definido
-            lt.pointLightOuterRadius=0.68f;
-            IsLightOn = false;// La luz se mantiene apagada mientras no se aprete "F"
+            col.radius = sizeRad; //El radio se mantiene al definido
+            lt.pointLightOuterRadius = 0.68f;
+            IsLightOn = false; //La luz se mantiene apagada mientras no se aprete "F"
         }
     }
 }
